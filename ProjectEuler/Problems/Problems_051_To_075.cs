f3418373362c881e786271849292bd6f5bc986d7
Zach Hollis <zhollis21@gmail.com>
2019-05-11 15:31:18 -0400

    Added a discription to problem 67

--- ProjectEuler/Problems/Problems_051_To_075.cs
+++ ProjectEuler/Problems/Problems_051_To_075.cs
@@ -123,7 +123,7 @@ namespace ProjectEuler.Problems
 
         public static void Problem67()
         {
-            Console.WriteLine("\n\n\n67. ");
+            Console.WriteLine("\n\n\n67. Find the maximum total from top to bottom in triangle.txt,\na 15K text file containing a triangle with one-hundred rows.");
 
             DateTime startTime = DateTime.Now;
             var triangle = GetTriangle();
