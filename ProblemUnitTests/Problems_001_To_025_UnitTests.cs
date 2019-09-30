614d4d79455efc6e1409baaad93b6f696f049382
Zach Hollis <zhollis21@gmail.com>
2019-09-29 21:47:58 -0400

    Added some tests around IsPrime.

--- ProblemUnitTests/Problems_001_To_025_UnitTests.cs
+++ ProblemUnitTests/Problems_001_To_025_UnitTests.cs
@@ -13,18 +13,43 @@ namespace ProblemUnitTests
         }
 
         [Test]
+        [TestCase(2, true)]
+        [TestCase(3, true)]
+        [TestCase(4, false)]
+        [TestCase(5, true)]
+        [TestCase(6, false)]
+        [TestCase(7, true)]
+        [TestCase(8, false)]
+        [TestCase(9, false)]
+        [TestCase(10, false)]
+        [TestCase(11, true)]
+        [TestCase(12, false)]
+        [TestCase(13, true)]
+        [TestCase(14, false)]
+        [TestCase(15, false)]
+        [TestCase(16, false)]
+        [TestCase(17, true)]
+        [TestCase(18, false)]
+        [TestCase(19, true)]
+        [TestCase(20, false)]
+        [TestCase(21, false)]
+        public void IsPrime_ReturnsExpectedResult(int number, bool expectedResult)
+        {
+            bool actualResult = Problems_001_To_025.IsPrime(number);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
         public void Problem001_ReturnsCorrectAnswer()
         {
             Assert.AreEqual("233168", Problems_001_To_025.Problem001());
         }
-
 
         [Test]
         public void Problem002_ReturnsCorrectAnswer()
         {
             Assert.AreEqual("4613732", Problems_001_To_025.Problem002());
         }
-
 
         [Test]
         public void Problem003_ReturnsCorrectAnswer()
